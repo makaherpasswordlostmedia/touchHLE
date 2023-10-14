7c2d1afd3789471b1a109b43b1afe34c4179f558
ciciplusplus <ciciplusplus@gmail.com>
2023-10-15 00:39:57 +0200

    WIP CFMutableDict
    Change-Id: I0fcb67f2cc2d0ea137cfe76525a4e2e86e119d4a

--- src/frameworks/core_foundation/cf_dictionary.rs
+++ src/frameworks/core_foundation/cf_dictionary.rs
@@ -8,6 +8,32 @@
 //! These are toll-free bridged to `NSDictionary` and `NSMutableDictionary` in
 //! Apple's implementation. Here they are the same types.
 
+use super::cf_allocator::{kCFAllocatorDefault, CFAllocatorRef};
+use super::CFIndex;
+use crate::dyld::{export_c_func, FunctionExports};
+use crate::frameworks::foundation::NSUInteger;
+use crate::mem::ConstVoidPtr;
+use crate::objc::{id, msg, msg_class};
+use crate::Environment;
+
 pub type CFDictionaryRef = super::CFTypeRef;
-#[allow(dead_code)]
 pub type CFMutableDictionaryRef = super::CFTypeRef;
+
+fn CFDictionaryCreateMutable(
+    env: &mut Environment,
+    allocator: CFAllocatorRef,
+    capacity: CFIndex,
+    keyCallbacks: ConstVoidPtr, // TODO, should be `const CFDictionaryKeyCallBacks*`
+    valueCallbacks: ConstVoidPtr, // TODO, should be `const CFDictionaryValueCallBacks*`
+) -> CFMutableDictionaryRef {
+    assert!(allocator == kCFAllocatorDefault); // unimplemented
+    assert!(capacity == 0); // TODO: fixed capacity support
+    assert!(keyCallbacks.is_null()); // TODO: support retaining etc
+    assert!(valueCallbacks.is_null()); // TODO: support retaining etc
+
+    crate::objc::nil
+}
+
+pub const FUNCTIONS: FunctionExports = &[
+    export_c_func!(CFDictionaryCreateMutable(_, _, _, _)),
+];
