630d941c3b0d19cf949511db8de080fe35776ff3
ciciplusplus <ciciplusplus@gmail.com>
2024-03-07 18:40:53 +0100

    thread 'main' panicked at 'Attempted null-page access at 0x0 (0x4 bytes)', src/mem.rs:337:9
    Change-Id: Ia8590b7117dac771dfbc1e82ec4066b033020811

--- src/frameworks/uikit/ui_image.rs
+++ src/frameworks/uikit/ui_image.rs
@@ -6,7 +6,7 @@
 //! `UIImage`.
 
 use crate::frameworks::core_graphics::cg_image::{self, CGImageRef, CGImageRelease, CGImageRetain};
-use crate::frameworks::core_graphics::CGSize;
+use crate::frameworks::core_graphics::{CGFloat, CGSize, CGPoint};
 use crate::frameworks::foundation::{ns_data, ns_string, NSInteger};
 use crate::fs::GuestPath;
 use crate::image::Image;
@@ -118,6 +118,13 @@ pub const CLASSES: ClassExports = objc_classes! {
         width: width as _,
         height: height as _,
     }
+}
+
+- (())drawAtPoint:(CGPoint)point
+        blendMode:(i32)blend_mode // CGBlendMode
+            alpha:(CGFloat)alpha {
+    log!("drawAtPoint p {} bm {} al {}", point, blend_mode, alpha);
+    assert_eq!(alpha, 0.0);
 }
 
 @end
