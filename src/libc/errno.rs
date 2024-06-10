0605a3e2a8a0f8afb8256e8838d4f0b08a54e7a7
makaherpasswordlostmedia <emssaoig1@gmail.com>
2024-06-10 17:38:07 +0300

    Update errno.rs

--- src/libc/errno.rs
+++ src/libc/errno.rs
@@ -6,30 +6,32 @@
 //! `errno.h`
 
 use crate::dyld::FunctionExports;
-use crate::environment::Environment;
 use crate::export_c_func;
-use crate::mem::{ConstPtr, MutPtr};
-use std::io::Write;
+use crate::mem::MutPtr;
+use crate::Environment;
 
 pub const EPERM: i32 = 1;
 pub const EDEADLK: i32 = 11;
+pub const EBUSY: i32 = 16;
 pub const EINVAL: i32 = 22;
 
 #[derive(Default)]
 pub struct State {
-    errnos: std::collections::HashMap<crate::ThreadID, MutPtr<i32>>,
+    errnos: std::collections::HashMap<crate::ThreadId, MutPtr<i32>>,
 }
 impl State {
     fn errno_for_thread(
         &mut self,
         mem: &mut crate::mem::Mem,
-        thread: crate::ThreadID,
+        thread: crate::ThreadId,
     ) -> MutPtr<i32> {
-        // TODO: "real" errno implementation
-        *self
-            .errnos
-            .entry(thread)
-            .or_insert_with(|| mem.alloc_and_write(0i32))
+        *self.errnos.entry(thread).or_insert_with(|| {
+            log!(
+                "TODO: errno accessed on thread {} (will always be 0)",
+                thread
+            );
+            mem.alloc_and_write(0i32)
+        })
     }
 }
 
@@ -39,10 +41,4 @@ fn __error(env: &mut Environment) -> MutPtr<i32> {
         .errno_for_thread(&mut env.mem, env.current_thread)
 }
 
-fn perror(env: &mut Environment, s: ConstPtr<u8>) {
-    // TODO: errno mapping
-    // TODO: null checks
-    let _ = std::io::stderr().write_all(env.mem.cstr_at(s));
-}
-
-pub const FUNCTIONS: FunctionExports = &[export_c_func!(__error()), export_c_func!(perror(_))];
+pub const FUNCTIONS: FunctionExports = &[export_c_func!(__error())];
